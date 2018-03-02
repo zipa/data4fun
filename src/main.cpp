b6eff6cbb802289cf37fe951d97ef371785546ca
Boris Petrovic <boris.petrovic@marvelsoft.net>
2018-03-02 23:25:49 +0100

    Added log ability
    Signed-off-by: Boris Petrovic <boris.petrovic@marvelsoft.net>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
+
+using wrapped_daily_sink = 
+        spdlog::sinks::daily_file_sink<spdlog::details::null_mutex, spdlog::sinks::dateonly_daily_file_name_calculator>;
 
 int main(int, char**)
 {
+    spdlog::set_pattern("%Y%m%d-%H:%M:%S.%f%z|%i|%L|%t|%n|%v");
+    auto dailySink = std::make_shared<wrapped_daily_sink>("log", 23, 59);
+    auto log = std::make_shared<spdlog::logger>(__FUNCTION__, dailySink);
+    log->critical("Testing {}", "What?");
     std::cout << "Game over.\n";
     return 0;
 }
